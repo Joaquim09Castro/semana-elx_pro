78351d892aa0ce715e0224d737f027a5251bb985
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-12-15 17:01:00 -0200

    test

--- lib/trello_tasker.ex
+++ lib/trello_tasker.ex
@@ -1,7 +1,7 @@
 defmodule TrelloTasker do
   @moduledoc """
   TrelloTasker keeps the contexts that define your domain
-  and business logic.
+  and business logic
 
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
